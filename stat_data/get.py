c50d1780496954e138ca8acb01e53a204ed100d4
taikob <taisukekbys@gmail.com>
2021-01-12 10:55:54 +0900

    add file_para

--- stat_data/get.py
+++ stat_data/get.py
@@ -1,21 +1,23 @@
 import numpy as np
 import re
+import os
+from natsort import natsorted
 
 def get_sysparam(data,paramrow):
 
-  param=list()
-  for i in paramrow:
-    prm=np.empty(0)
-    for j in range(len(data)):
-      if not float(data[j][i]) in prm:
-        prm=np.append(prm,float(data[j][i]))
-    param.append(np.sort(prm))
+    param=list()
+    for i in paramrow:
+        prm=np.empty(0)
+        for j in range(len(data)):
+            if not float(data[j][i]) in prm:
+                prm=np.append(prm,float(data[j][i]))
+        param.append(np.sort(prm))
 
-  nump=[]
-  for p in param:
-    nump.append(len(p))
+    nump=[]
+    for p in param:
+        nump.append(len(p))
 
-  return param,nump
+    return param,nump
 
 def readtitleparam(title):
 
@@ -28,3 +30,24 @@ def readtitleparam(title):
         titleparam.append(re.sub('\\D','',t))
 
     return [re.findall(pattern, t)[0] for t in title]
+
+def file_para(path,comtxt,title=None):
+    #comtxt : common text
+    #paramum : parameter number
+
+    for dir in natsorted(os.listdir(path)):
+        if comtxt in dir:
+            tprm = readtitleparam(dir)
+            paramnum = len(tprm)
+            break
+
+    out = np.empty((0, paramnum + 1))
+    for dir in natsorted(os.listdir(path)):
+        if comtxt in dir:
+            data = float(np.loadtxt(path + '/' + dir))
+            tprm = readtitleparam(dir)
+            tprm.append(data)
+            out = np.append(out, np.array([[tprm]], axis=0))
+
+    if title is None: title='stat_data.csv'
+    np.savetxt(path + '/' + title, out, delimiter=',')
